d97f15560258bc16d5c4d5f16906931c3933c447
Kozimjondev <developer.0829.@gmail.com>
2022-07-26 10:39:29 +0500

    Update 12-xatolar bn ishlash.py
    12-dars yangilandi

--- 12-xatolar bn ishlash.py
+++ 12-xatolar bn ishlash.py
@@ -2,8 +2,8 @@
 # print ("Hello World")
 # print("Hello world)
 
-# son=int(input("son kiriting: "))
-# print(f"{son} ning kvadrati {son**2} ga teng")
+son=int(input("son kiriting: "))
+print(f"{son} ning kvadrati {son**2} ga teng")
 
 radius = 5
 pi = 5.1
