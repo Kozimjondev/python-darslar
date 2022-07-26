ef68e2993b6f32c7ac81ac5d51039713aed061a8
Kozimjondev <developer.0829.@gmail.com>
2022-07-26 10:34:26 +0500

    Create 12-xatolar bn ishlash.py
    12-dars qo'shildi

---
+++ 12-xatolar bn ishlash.py
@@ -0,0 +1,25 @@
+# Syntax Error
+# print ("Hello World")
+# print("Hello world)
+
+# son=int(input("son kiriting: "))
+# print(f"{son} ning kvadrati {son**2} ga teng")
+
+radius = 5
+pi = 5.1
+aylana_yuzi = pi*radius**2
+print(aylana_yuzi)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
